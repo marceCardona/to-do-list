5d40e9867286b4002a51d810d286b0994df148c2
Marcela <marcela.cardona.c@gmail.com>
2020-03-25 15:27:38 +1100

    Finish JS functionality and add icons source

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-const buttonAdd = document.getElementById('myButton');
-
+//Get the task text from the input, if there is text then call the addNewTask function
 const getTask = () => {
     var task = document.getElementById("task").value;
     task ? addNewTask(task) : null;
+    document.getElementById("task").value = "";
 }
 
+//create the html for each new task and add it to the list
 const addNewTask = (str) => {
     var newLi = document.createElement("li");
     var newCheck = document.createElement("input");
     newCheck.setAttribute("type", "checkbox");
-//    newCheck.setAttribute("onclick", "checked()");
+    var newIcon = document.createElement("span");
+    newIcon.setAttribute("class", "fas fa-trash-alt");
     var newtask = document.createTextNode(str);
     newLi.appendChild(newCheck);
     newLi.appendChild(newtask);
+    newLi.appendChild(newIcon);
 
     var list = document.getElementById("task-list");
     list.appendChild(newLi);
-    list.addEventListener("click", checked, false)
+    list.addEventListener("click", checked, false);
 }
 
+//change classes when the checkbox ot the delete icon are clicked
 const checked = (e) => {
-   console.log(e.target);
+   if (e.target.tagName === "INPUT"){
+       e.target.classList.toggle('checked');
+   } else if(e.target.tagName === "SPAN"){
+       e.target.parentElement.style.display = "none";
+       //classList.add('deleted');
+   }
 }
 
+const buttonAdd = document.getElementById('myButton');
 buttonAdd.onclick = () => { getTask(); };
